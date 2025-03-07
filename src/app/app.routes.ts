39dbdb96125df985b40135a335c352d3a43d013e
Syed Rafiul Kabir <rafiulkabir01.rucse@gmail.com>
2025-03-07 22:28:06 +0600

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: 'customer',
+		loadChildren: () => import('./customer/customer.module').then(m => m.CustomerModule)
+	},
+	{ path: '', redirectTo: '/customer', pathMatch: 'full' }
+];
---
+++ src/app/customer/customer.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CustomerRoutingModule } from './customer-routing.module';
+import { CustomerFormComponent } from './customer-form/customer-form.component';
+
+
+@NgModule({
+    declarations: [
+        CustomerFormComponent
+    ],
+    imports: [
+        CommonModule,
+        CustomerRoutingModule
+    ]
+})
+export class CustomerModule { }
